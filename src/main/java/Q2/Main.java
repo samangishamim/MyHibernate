f0dbf53937255636858a4d11533e78e96ec6e90c
samangishamim <samangi624@gmail.com>
2024-04-03 17:48:22 +0430

    create new class

--- src/main/java/Q2/Main.java
+++ src/main/java/Q2/Main.java
@@ -1,4 +1,13 @@
 package Q2;
 
+import Q2.model.Person;
+import Q2.repository.PersonRepository;
+
 public class Main {
+    public static void main(String[] args) {
+        PersonRepository personRepository = new PersonRepository();
+        personRepository.save(new Person("shamim","samangi",1999));
+    }
+
+
 }
---
+++ src/main/java/Q2/connection/SessionFactorySingleton.java
@@ -0,0 +1,29 @@
+package Q2.connection;
+
+import Q2.model.Person;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class SessionFactorySingleton {
+    private SessionFactorySingleton() {
+    }
+
+    private static class LazyHolder {
+        static SessionFactory INSTANCE;
+
+        static {
+            var registry = new StandardServiceRegistryBuilder()
+                    .configure()
+                    .build();
+            INSTANCE = new MetadataSources(registry)
+                    .addAnnotatedClass(Person.class)
+                    .buildMetadata()
+                    .buildSessionFactory();
+        }
+    }
+
+    public static SessionFactory getInstance() {
+        return LazyHolder.INSTANCE;
+    }
+}
