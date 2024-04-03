05dbb4ea624d699f504ecedda67ab803b6ab4efc
samangishamim <samangi624@gmail.com>
2024-04-03 14:16:30 +0430

    write the code for testing the add method

--- src/test/java/Q1/RationalTest.java
+++ src/test/java/Q1/RationalTest.java
@@ -8,11 +8,15 @@ class RationalTest {
 
     @Test
     void testAdd() {
-
+        Rational r1 = new Rational(1, 2);
+        Rational r2 = new Rational(1, 3);
+        Rational result = r1.add(r2);
+        assertEquals("5/6", result.toString());
     }
 
     @Test
     void testSub() {
+
     }
 
     @Test
