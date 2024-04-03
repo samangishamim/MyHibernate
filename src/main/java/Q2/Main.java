582886fc683a7d17e0f842f61caecb934458cb1f
samangishamim <samangi624@gmail.com>
2024-04-03 18:21:40 +0430

    write code for test

--- src/main/java/Q2/Main.java
+++ src/main/java/Q2/Main.java
@@ -6,7 +6,12 @@ import Q2.repository.PersonRepository;
 public class Main {
     public static void main(String[] args) {
         PersonRepository personRepository = new PersonRepository();
-        personRepository.save(new Person("shamim","samangi",1999));
+//        personRepository.save(new Person("shamim","samangi",1999));
+//        personRepository.save(new Person("reyhane","darvishian",2000));
+
+//        System.out.println(personRepository.findById(1l));
+
+
     }
 
 
