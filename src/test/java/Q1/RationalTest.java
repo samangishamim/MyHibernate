a5d82302d93a9dc496ba6bea11c357f389c4939d
samangishamim <samangi624@gmail.com>
2024-04-03 14:15:11 +0430

    create test class for getting all the test

---
+++ src/test/java/Q1/RationalTest.java
@@ -0,0 +1,29 @@
+package Q1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RationalTest {
+
+    @Test
+    void testAdd() {
+
+    }
+
+    @Test
+    void testSub() {
+    }
+
+    @Test
+    void testMul() {
+    }
+
+    @Test
+    void testDiv() {
+    }
+
+    @Test
+    void testToFloatingPoint() {
+    }
+}
