2d7956ad9e062965e8e4580ea41511a81d16d1cc
samangishamim <samangi624@gmail.com>
2024-04-03 23:25:52 +0430

    add new constructor just for id

--- src/main/java/Q2/model/Person.java
+++ src/main/java/Q2/model/Person.java
@@ -1,31 +1,35 @@
 package Q2.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-@ToString
 
-@Entity
+@Entity(name = "Person")
+@Table(name = "person")
 public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String familyName;
-    private Integer birthdate;
+    private LocalDate birthdate;
 
-    public Person(String name, String familyName,Integer birthdate) {
+    public Person(String name, String familyName,LocalDate birthdate) {
+        this.name = name;
+        this.familyName = familyName;
+        this.birthdate = birthdate;
+    }
+
+    public Person() {
+    }
+
+    public Person(Long id, String name, String familyName, LocalDate birthdate) {
+        this.id = id;
         this.name = name;
         this.familyName = familyName;
         this.birthdate = birthdate;
@@ -33,5 +37,47 @@ public class Person {
 
     public Person(Long id) {
 
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", familyName='" + familyName + '\'' +
+                ", birthdate=" + birthdate +
+                '}';
     }
 }
