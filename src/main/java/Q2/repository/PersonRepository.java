5883b48f329015c47c1a46875fe77330c5766b0b
samangishamim <samangi624@gmail.com>
2024-04-03 15:58:18 +0430

    add save method

--- src/main/java/Q2/repository/PersonRepository.java
+++ src/main/java/Q2/repository/PersonRepository.java
@@ -1,4 +1,29 @@
 package Q2.repository;
 
+import Q2.model.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 public class PersonRepository {
+
+    StandardServiceRegistry build=new StandardServiceRegistryBuilder()
+            .configure()
+            .build();
+
+    SessionFactory sessionFactory=new MetadataSources(build)
+            .addAnnotatedClass(Person.class)
+            .buildMetadata().buildSessionFactory();
+
+
+    public void save(Person person){
+        Session session= sessionFactory.openSession();
+        Transaction transaction= session.beginTransaction();
+        session.persist(person);
+        transaction.commit();
+        session.close();
+    }
 }
