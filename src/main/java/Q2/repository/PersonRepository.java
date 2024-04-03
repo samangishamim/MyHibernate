f129dd0266bb2f3aa2ace4efd8fc0cd99d07337a
samangishamim <samangi624@gmail.com>
2024-04-03 23:25:19 +0430

    fix the all the methods and add the contain method

--- src/main/java/Q2/repository/PersonRepository.java
+++ src/main/java/Q2/repository/PersonRepository.java
@@ -5,60 +5,66 @@ import Q2.model.Person;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.query.Query;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class PersonRepository {
-    private final SessionFactory sessionFactory = SessionFactorySingleton.getInstance();
+    private final SessionFactory factory = SessionFactorySingleton.getInstance();
 
     public void save(Person person) {
-        Session currentSession = sessionFactory.openSession();
-        Transaction transaction = currentSession.beginTransaction();
-        currentSession.persist(person);
-        transaction.commit();
-        currentSession.close();
+        Session session = factory.openSession();
+        session.getTransaction().begin();
+
+        session.persist(person);
+
+        session.getTransaction().commit();
+        session.close();
     }
 
     public Person findById(Long id) {
-        Session currentSession = sessionFactory.openSession();
-        Transaction transaction = currentSession.beginTransaction();
+        Session currentSession = factory.openSession();
         Person person = currentSession.get(Person.class, id);
-        transaction.commit();
-        sessionFactory.close();
         return person;
     }
 
     public List<Person> findAll() {
-        Session currentSession = sessionFactory.openSession();
-        Transaction transaction = currentSession.beginTransaction();
-        String sql="from person";
+        Session currentSession = factory.openSession();
+        String sql = "from Person";
         Query<Person> fromFilm = currentSession.createQuery(sql, Person.class);
         List<Person> listOfFilm = fromFilm.list();
-        transaction.commit();
-        currentSession.close();
         return listOfFilm;
     }
 
 
     public void remove(Person person) {
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
+        Session session = factory.openSession();
+        session.getTransaction().begin();
+
         session.remove(person);
-        transaction.commit();
+
+        session.getTransaction().commit();
         session.close();
     }
 
     public void update(Person person) {
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
+        Session session = factory.openSession();
+        session.getTransaction().begin();
+
         session.merge(person);
-        transaction.commit();
+
+        session.getTransaction().commit();
         session.close();
+    }
+
+    public boolean contain(Long id) {
+        Session session = factory.openSession();
+
+        Person person = session.find(Person.class,id);
+
+        boolean resualt = session.contains(person);
+        session.close();
+        return resualt;
     }
 
 }
