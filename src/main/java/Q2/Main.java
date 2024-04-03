2ab211a09fd8e6ae98fd029c00d59b49f168714a
samangishamim <samangi624@gmail.com>
2024-04-03 23:24:26 +0430

    add the signUp method and fix the all code

--- src/main/java/Q2/Main.java
+++ src/main/java/Q2/Main.java
@@ -2,16 +2,42 @@ package Q2;
 
 import Q2.model.Person;
 import Q2.repository.PersonRepository;
+import Q2.service.PersonService;
+
+import java.time.LocalDate;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        PersonRepository personRepository = new PersonRepository();
-//        personRepository.save(new Person("shamim","samangi",1999));
-//        personRepository.save(new Person("reyhane","darvishian",2000));
+        PersonService personService = new PersonService();
+        Scanner scanner=new Scanner(System.in);
 
-//        System.out.println(personRepository.findById(1l));
+        signUp(scanner, personService);
+
+//        personService.save(new Person("reyhane","darvishian",LocalDate.parse("1999-11-03")));
+
+//        System.out.println(personService.findById(1l));
+
+//        personService.findAll().forEach(System.out::println);
+
+//        personService.remove(3l);
+
+//        personService.update(new Person(4L,"asghar","shalgham",LocalDate.parse("1990-12-04")));
+
+//        System.out.println(personService.contain(10L));
+    }
+
+    private static void signUp(Scanner scanner, PersonService personService) {
+        System.out.println("enter your first name : ");
+        String firstName = scanner.nextLine();
+        System.out.println("enter your last name : ");
+        String lastName = scanner.nextLine();
+        System.out.println("enter your birthdate : ");
+        String dateStr = scanner.nextLine();
+        LocalDate birthdate = LocalDate.parse(dateStr);
 
 
+        personService.signUp(new Person(firstName, lastName, birthdate));
     }
 
 
